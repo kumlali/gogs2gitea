1dc03497567708517c8036f35d9b078598443b07
kumlali <as_kumlali@yahoo.com>
2017-12-19 14:48:01 +0300

    Refactoring, new functions and documentation.

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # gogs2gitea
 Migration script that helps copy organizations and repositories(with all the history) from Gogs/Gitea to Gogs/Gitea.
 
-## What it does?
+## What does it do?
 * Creates all the organizations of source Gogs/Gitea on destination Gogs/Gitea
 * Creates all the repositories of each organization of source Gogs/Gitea on destination Gogs/Gitea
 * Migrates repositories of source Gogs/Gitea to destination Gogs/Gitea
 
 ## Limitations
-* It does not migrate specific user's organization and repositories. You can use `copy_repo()` function, though.
+* It does not migrate specific user's organization and repositories. You can use `copy_user_repo()` function, though.
 * It does not create users on destination Gogs/Gitea
-* It does not create teams on destination Gogs/Gitea. Owner of all the organizations and repositories is `dst_owner_user`.
+* It does not create teams on destination Gogs/Gitea. Authenticated user of destination server(`dst_owner_user`) becomes the owner of all the organizations and repositories.
 * It does not copy avatars from source Gogs/Gitea to destination Gogs/Gitea.
 
 ## Compatibility
@@ -17,11 +17,11 @@ Migration script that helps copy organizations and repositories(with all the his
 Tested with;
 * Python 2.7.5
 * Gogs Version: 0.11.4.0405
-* Gitea Version: 1.3.0
+* Gitea Version: 1.3.0 and 1.3.2
 
 ## Prerequisites
 * An administrator account and its access token must be created on destination Gogs/Gitea to be used as `dst_owner_user`.
-* Access token of source must belong to a user which owns all the organizations and repositories. Otherwise, organizations and repositories of the given user are copied only.
+* Access token of source must belong to a user which owns all the organizations and repositories. Otherwise, given user's organizations and repositories are copied only.
 
 ## Hints
 
@@ -34,11 +34,8 @@ gitea_url = 'http://gitea.mycompany.com'
 gitea_token = '5d7da2370c4b62c99f5c6496391a2ed66f07be3f'
 owner_user = 'gitea_admin'
 
-# Prints organizations and repos of Gogs
+# Prints organizations and repos
 print_orgs_and_repos(gogs_url, gogs_token)
-
-# Prints organizations and repos of Gitea
-print_orgs_and_repos(gitea_url, gitea_token)
 
 # Creates newOrg organization whose owner ise owner_user
 create_org(gitea_url, 
@@ -50,27 +47,39 @@ create_org(gitea_url,
            'http://newOrg.mycompany.com', 
            'TR')
 
-# Creates newRepo repository under newOrg organization
+# Creates newRepo repository under newOrg organization.
 create_repo(gitea_url, gitea_token, 'newOrg', 'newRepo')
 
-# Migrates newRepo of newOrg from Gogs to Gitea
-migrate_repo(gogs_url, gogs_token, gitea_url, gitea_token, 'newOrg', 'newRepo')
+# Migrates newRepo of newOrg from Gogs to Gitea.
+migrate_repo(gogs_url, gogs_token, 'newOrg', gitea_url, gitea_token, 'newOrg', 'newRepo')
 
-# Deletes newRepo of newOrg
+# Deletes newRepo of newOrg.
 delete_repo(gitea_url, gitea_token, 'newOrg', 'newRepo')
 
 # Copies testOrg of Gogs to Gitea. Repositories are not copied.
 copy_org(gogs_url, gogs_token, gitea_url, gitea_token, owner_user, 'testOrg')
 
-# Copies testRepo of testOrg from Gogs to Gitea.
-# It simply does create_repo() and migrate_repo().
-copy_repo(gogs_url, gogs_token, gitea_url, gitea_token, 'testOrg', 'testRepo')
+# Copies testRepo of gogsOrg from Gogs to giteaOrg of Gitea.
+# It simply calls create_repo() and migrate_repo().
+copy_repo(gogs_url, gogs_token, 'gogsOrg', gitea_url, gitea_token, 'giteaOrg', 'testRepo')
 
-# Copies all the organizations from Gogs to Gitea
-copy_all_orgs(gogs_url, gogs_token, gitea_url, gitea_token, owner_user)
+# Copies all the organizations from Gogs to Gitea. gitea_owner_user
+# becomes the owner of all the organizations.
+copy_orgs(gogs_url, gogs_token, gitea_url, gitea_token, gitea_owner_user)
 
-# Copies all the repositories of each organization on Gogs to Gitea
-copy_all_repos(gogs_url, gogs_token, gitea_url, gitea_token)
+# Copies all the repositories of each organization on Gogs to Gitea.
+# All the Gogs organizations must exist on Gitea. copy_orgs() can be used 
+# for this.
+copy_repos(gogs_url, gogs_token, gitea_url, gitea_token)
+
+# Copies MyRepo of MyUser under to YourUser in the same server.
+copy_user_repo(gitea_url, gitea_token, 'MyUser', gitea_url, gitea_token, 'YourUser', 'MyRepo')
+
+# Copies public repositories of MyUser on Gogs under to YourUser on Gitea. 
+copy_user_repos(gogs_url, gogs_token, 'MyUser', gitea_url, gitea_token, 'YourUser')
+
+# Prints repositories of YourUser
+print_user_repos(gitea_url, gitea_token, 'YourUser')
 ```
 
 ## Usage
--- migrate.py
+++ migrate.py
@@ -44,24 +44,24 @@ def parse_arguments():
     arg_dst_owner_user = args.dst_owner_user
 
 def get(url, params, token):
-   request = urllib2.Request(url + "?" + urllib.urlencode(params))
-   request.add_header("Content-Type",'application/json')
-   request.add_header("Authorization", 'token ' + token)
-   return json.load(urllib2.urlopen(request))
+    request = urllib2.Request(url + "?" + urllib.urlencode(params))
+    request.add_header("Content-Type",'application/json')
+    request.add_header("Authorization", 'token ' + token)
+    return json.load(urllib2.urlopen(request))
 
 def post(url, params, token):
-   request = urllib2.Request(url, data=json.dumps(params))
-   print json.dumps(params)
-   request.add_header("Content-Type",'application/json')
-   request.add_header("Authorization", 'token ' + token)
-   return urllib2.urlopen(request)
+    request = urllib2.Request(url, data=json.dumps(params))
+    print json.dumps(params)
+    request.add_header("Content-Type",'application/json')
+    request.add_header("Authorization", 'token ' + token)
+    return urllib2.urlopen(request)
 
 def delete(url, token):
-   request = urllib2.Request(url)
-   request.add_header("Content-Type",'application/json')
-   request.add_header("Authorization", 'token ' + token)
-   request.get_method = lambda: 'DELETE'
-   return urllib2.urlopen(request)
+    request = urllib2.Request(url)
+    request.add_header("Content-Type",'application/json')
+    request.add_header("Authorization", 'token ' + token)
+    request.get_method = lambda: 'DELETE'
+    return urllib2.urlopen(request)
 
 # http://gogs.mycompany.com -> http://gogs.mycompany.com/api/v1
 def get_api_url(url):
@@ -69,9 +69,9 @@ def get_api_url(url):
 
 # http(s)://gogs.mycompany.com -> http(s)://<token>@gogs.mycompany.com
 def get_token_inserted_url(url, token):
-   first = url.split('/')[0] + '//'
-   last = '@' + url.split('/')[2]
-   return first + token + last
+    first = url.split('/')[0] + '//'
+    last = '@' + url.split('/')[2]
+    return first + token + last
 
 """
 Equivalent to:
@@ -97,7 +97,7 @@ Equivalent to:
        http://gogs.mycompany.com/api/v1/user/orgs
 """
 def get_orgs(url, token):
-   return get(get_api_url(url) + '/user/orgs', '', token)
+    return get(get_api_url(url) + '/user/orgs', '', token)
 
 """
 Equivalent to:
@@ -108,15 +108,15 @@ Equivalent to:
         http://gogs.mycompany.com/api/v1/admin/users/<owner_user_of_NewOrg>/orgs
 """
 def create_org(url, token, owner_user, org, full_name='', description='', website='', location=''):
-   print 'Organization will be created -> On Git server: ' + url + ', Owner user: ' + owner_user + ', Organization: ' + org
-   if get_org(url, token, org) != '':
-       raise Exception('Organization [' + org + '] already exists on ' + url) 
-   post(get_api_url(url) + '/admin/users/' + owner_user + '/orgs', {'username': org, 
-                                                                     'full_name': full_name, 
-                                                                     'description': description, 
-                                                                     'website': website, 
-                                                                     'location': location}, token)
-   print 'Organization has been created -> On Git server: ' + url + ', Owner user: ' + owner_user + ', Organization: ' + org
+    print 'Organization will be created -> On Git server: ' + url + ', Owner user: ' + owner_user + ', Organization: ' + org
+    if get_org(url, token, org) != '':
+        raise Exception('Organization [' + org + '] already exists on ' + url) 
+    post(get_api_url(url) + '/admin/users/' + owner_user + '/orgs', {'username': org, 
+                                                                        'full_name': full_name, 
+                                                                        'description': description, 
+                                                                        'website': website, 
+                                                                        'location': location}, token)
+    print 'Organization has been created -> On Git server: ' + url + ', Owner user: ' + owner_user + ', Organization: ' + org
 
 """
 Copies given org on source server to destination server. Repositories 
@@ -155,26 +155,31 @@ def copy_org(src_url, src_token, dst_url, dst_token, dst_owner_user, org):
 """
 Copies all the organizations on source server to destination server.
 
+Repositories are not copied.
+
 Because /orgs/<orgname> (and get_org() accordingly) does not return 
 the owner, we need to know it while creating the organization on 
 destination server. That is why we need dst_owner_user.
 """
-def copy_all_orgs(src_url, src_token, dst_url, dst_token, dst_owner_user):
+def copy_orgs(src_url, src_token, dst_url, dst_token, dst_owner_user):
     orgs = get_orgs(src_url, src_token)
     for org in orgs:
         orgName = org['username']
         copy_org(src_url, src_token, dst_url, dst_token, dst_owner_user, orgName)
 
 """
+Returns given repository of given owner. Owner can either be an organization or
+a user.
+
 Equivalent to:
     curl -v \
        -H "Content-Type: application/json" \
        -H "Authorization: token <token>" \
-       http://gogs.mycompany.com/api/v1/repos/MyOrg/MyRepo
+       http://gogs.mycompany.com/api/v1/repos/MyOrgOrUser/MyRepo
 """
-def get_repo(url, token, org, repo):
+def get_repo(url, token, owner, repo):
     try:
-       return get(get_api_url(url) + '/repos/' + org + '/' + repo, '', token)
+       return get(get_api_url(url) + '/repos/' + owner + '/' + repo, '', token)
     except urllib2.HTTPError as err:
        if err.code == 404:
            return ''
@@ -189,9 +194,23 @@ Equivalent to:
        http://gogs.mycompany.com/api/v1/orgs/MyOrg/repos
 """
 def get_repos(url, token, org):
-   return get(get_api_url(url) + '/orgs/' + org + '/repos', '', token)
+    return get(get_api_url(url) + '/orgs/' + org + '/repos', '', token)
 
 """
+Returns repositories of given user.
+
+Equivalent to:
+    curl -v \
+       -H "Content-Type: application/json" \
+       -H "Authorization: token <token>" \
+       http://gogs.mycompany.com/api/v1/users/MyUser/repos
+"""
+def get_user_repos(url, token, user):
+    return get(get_api_url(url) + '/users/' + user + '/repos', '', token)
+
+"""
+Creates given repository under given organization.
+
 Equivalent to:
     curl -v -X POST \
        -H "Content-Type: application/json" \
@@ -200,13 +219,30 @@ Equivalent to:
        http://gitea.mycompany.com/api/v1/org/MyOrg/repos
 """
 def create_repo(url, token, org, repo):
-   print 'Repository will be created -> On Git server: ' + url + ', Organization: ' + org + ', Repository: ' + repo
-   if get_org(url, token, org) == '':
-       raise Exception('Organization [' + org + '] does not exist on ' + url) 
-   if get_repo(url, token, org, repo) != '':
-       raise Exception('Repository [' + repo + '] of organization [' + org + '] already exists on ' + url)
-   post(get_api_url(url) + '/org/' + org + '/repos', {'name': repo}, token)
-   print 'Repository has been created -> On Git server: ' + url + ', Organization: ' + org + ', Repository: ' + repo
+    print 'Repository will be created -> On Git server: ' + url + ', Organization: ' + org + ', Repository: ' + repo
+    if get_org(url, token, org) == '':
+        raise Exception('Organization [' + org + '] does not exist on ' + url) 
+    if get_repo(url, token, org, repo) != '':
+        raise Exception('Repository [' + repo + '] of organization [' + org + '] already exists on ' + url)
+    post(get_api_url(url) + '/org/' + org + '/repos', {'name': repo}, token)
+    print 'Repository has been created -> On Git server: ' + url + ', Organization: ' + org + ', Repository: ' + repo
+
+"""
+Creates repository for authenticated user whose token is used.
+
+Equivalent to:
+    curl -v -X POST \
+       -H "Content-Type: application/json" \
+       -H "Authorization: token <token>" \
+       -d '{"name":"MyUserRepo"}' \
+       http://gitea.mycompany.com/api/v1/user/repos
+"""
+def create_user_repo(url, token, user, repo):
+    print 'User repository will be created -> On Git server: ' + url + ', Authenticated User, Repository: ' + repo
+    if get_repo(url, token, user, repo) != '':
+        raise Exception('Repository [' + repo + '] of user [' + user + '] already exists on ' + url)
+    post(get_api_url(url) + '/user/repos', {'name': repo}, token)
+    print 'User repository has been created -> On Git server: ' + url + ', Authenticated User, Repository: ' + repo
 
 """
 Equivalent to:
@@ -216,16 +252,28 @@ Equivalent to:
        http://gitea.mycompany.com/api/v1/repos/MyOrg/MyRepo 
 """
 def delete_repo(url, token, org, repo):
-   print 'Repository will be deleted -> From Git server: ' + url + ', Organization: ' + org + ', Repository: ' + repo
-   if get_org(url, token, org) == '':
-       raise Exception('Organization [' + org + '] does not exist on ' + url) 
-   if get_repo(url, token, org, repo) == '':
-       print 'Repository [' + repo + '] of organization [' + org + '] does not exist on ' + url
-   else:
-       delete(get_api_url(url) + '/repos/' + org + '/' + repo, token)
-       print 'Repository has been deleted -> From Git server: ' + url + ', Organization: ' + org + ', Repository: ' + repo
+    print 'Repository will be deleted -> From Git server: ' + url + ', Organization: ' + org + ', Repository: ' + repo
+    if get_org(url, token, org) == '':
+        raise Exception('Organization [' + org + '] does not exist on ' + url) 
+    if get_repo(url, token, org, repo) == '':
+        print 'Repository [' + repo + '] of organization [' + org + '] does not exist on ' + url
+    else:
+        delete(get_api_url(url) + '/repos/' + org + '/' + repo, token)
+        print 'Repository has been deleted -> From Git server: ' + url + ', Organization: ' + org + ', Repository: ' + repo
 
-def delete_all_repos(url, token):
+"""
+Deletes all repositories of given organization.
+"""
+def delete_repos(url, token, org):
+    repos = get_repos(url, token, org)
+    for repo in repos:
+        repoName = repo['name']
+        delete_repo(url, token, org, repoName)
+
+"""
+Deletes all repositories of each organization.
+"""
+def delete_repos(url, token):
     orgs = get_orgs(url, token)
     for org in orgs:
         orgName = org['username']
@@ -234,7 +282,12 @@ def delete_all_repos(url, token):
             repoName = repo['name']
             delete_repo(url, token, orgName, repoName)
 
-def create_all_repos(src_url, src_token, dst_url, dst_token):
+"""
+Creates organization repositories of source server under the same organization 
+of destination server. It does not copy the content of the repositories(See 
+copy_repos()) 
+"""
+def create_repos(src_url, src_token, dst_url, dst_token):
     orgs = get_orgs(src_url, src_token)
     for org in orgs:
         orgName = org['username']
@@ -244,70 +297,120 @@ def create_all_repos(src_url, src_token, dst_url, dst_token):
             create_repo(dst_url, dst_token, orgName, repoName)
 
 """
+Migrates repository of an owner(organizaton or user) on source server under to 
+an owner on destination server.
+
 Equivalent to:
-    $ git -c http.sslVerify=false clone --bare http://<token>@gogs.mycompany.com/MyOrg/MyRepo.git
+    $ git -c http.sslVerify=false clone --bare http://<token>@gogs.mycompany.com/src_owner/MyRepo.git
     $ cd MyRepo.git
-    $ git -c http.sslVerify=false push --mirror http://<token>@gitea.mycompany.com/MyOrg/MyRepo.git
+    $ git -c http.sslVerify=false push --mirror http://<token>@gitea.mycompany.com/dst_owner/MyRepo.git
     $ rm -rf MyRepo.git
 """
-def migrate_repo(src_url, src_token, dst_url, dst_token, org, repo):
-    print 'Repository migration has been started -> From:' + src_url + ', To: ' + dst_url + ', Organization: ' + org + ', Repository: ' + repo
+def migrate_repo(src_url, src_token, src_owner, dst_url, dst_token, dst_owner, repo):
+    print 'Repository migration has been started -> From:' + src_url + ', To: ' + dst_url + ', Source owner: ' + src_owner + ', Destination owner: ' + dst_owner + ', Repository: ' + repo
 
     repo_dir = repo + '.git'
     if os.path.exists(repo_dir):
         print repo_dir + ' is already exist. It will be deleted...'
         subprocess.check_call(['rm', '-rf', repo_dir])
 
-    src_repo_url = get_token_inserted_url(src_url, src_token) + '/' + org + '/' + repo + '.git'
-    dst_repo_url = get_token_inserted_url(dst_url, dst_token) + '/' + org + '/' + repo + '.git'
+    src_repo_url = get_token_inserted_url(src_url, src_token) + '/' + src_owner + '/' + repo + '.git'
+    dst_repo_url = get_token_inserted_url(dst_url, dst_token) + '/' + dst_owner + '/' + repo + '.git'
 
     subprocess.check_call(['git', '-c', 'http.sslVerify=false', 'clone', '--bare', src_repo_url])
     os.chdir(repo_dir)
     subprocess.check_call(['git', '-c', 'http.sslVerify=false', 'push', '--mirror', dst_repo_url])
     os.chdir('..')
     subprocess.check_call(['rm', '-rf', repo_dir]) 
-    print 'Repository migration has been completed -> From:' + src_url + ', To: ' + dst_url + ', Organization: ' + org + ', Repository: ' + repo
+    print 'Repository migration has been completed -> From:' + src_url + ', To: ' + dst_url + ', Source owner: ' + src_owner + ', Destination owner: ' + dst_owner + ', Repository: ' + repo
 
 """
-Copies given repo of given org on source server to destination server.
-
-It simply does create_repo() and migrate_repo().
+Copies given repository under any organization of source server on to any 
+organization of destination server.
 """
-def copy_repo(src_url, src_token, dst_url, dst_token, org, repo):
-    if get_org(src_url, src_token, org) == '':
-        raise Exception('Organization [' + org + '] does not exist on ' + src_url) 
-    if get_repo(src_url, src_token, org, repo) == '':
-        raise Exception('Repository [' + repo + '] of organization [' + org + '] does not exist on ' + src_url)
+def copy_repo(src_url, src_token, src_org, dst_url, dst_token, dst_org, repo):
+    if get_org(src_url, src_token, src_org) == '':
+        raise Exception('Organization [' + src_org + '] does not exist on ' + src_url) 
+    if get_repo(src_url, src_token, src_org, repo) == '':
+        raise Exception('Repository [' + repo + '] of organization [' + src_org + '] does not exist on ' + src_url)
 
-    create_repo(dst_url, dst_token, org, repo)
-    migrate_repo(src_url, src_token, dst_url, dst_token, org, repo)
+    create_repo(dst_url, dst_token, dst_org, repo)
+    migrate_repo(src_url, src_token, src_org, dst_url, dst_token, dst_org, repo)
 
 """
-   Copies all the repositories of each organization on source server 
-   to destination server.
-   
-   All the organizations of source server must exist on destination 
-   server. copy_all_orgs() can be called for this.
+Copies all the repositories of each organization on source server 
+under to the same organizations of destination server.
+
+All the organizations of source server must exist on destination 
+server. copy_orgs() can be used for this.
 """
-def copy_all_repos(src_url, src_token, dst_url, dst_token):
+def copy_repos(src_url, src_token, dst_url, dst_token):
     orgs = get_orgs(src_url, src_token)
     for org in orgs:
         orgName = org['username']
         repos = get_repos(src_url, src_token, orgName)
         for repo in repos:
             repoName = repo['name']
-            copy_repo(src_url, src_token, dst_url, dst_token, orgName, repoName)
+            copy_repo(src_url, src_token, orgName, dst_url, dst_token, orgName, repoName)
+
+"""
+Copies given public repository of a user on source server under to user having 
+dst_token(authenticated user) on destination server.
+
+Nothing is done if repository is private.
+
+If dst_user and dst_token do not match, repository is created under user 
+having dst_token and migration fails. If API allowed administrators to create 
+a repository under *any* user, we would not have this shortcoming.
+"""
+def copy_user_repo(src_url, src_token, src_user, dst_url, dst_token, dst_user, repo):
+    src_repo = get_repo(src_url, src_token, src_user, repo)
+    if src_repo == '':
+        raise Exception('Repository [' + repo + '] of user [' + src_user + '] does not exist on ' + src_url)
+    if src_repo['private']:
+        print 'User repository [' + repo + '] is private. It will not be copied. Please make it public and retry.'
+    else:        
+        create_user_repo(dst_url, dst_token, dst_user, repo)
+        migrate_repo(src_url, src_token, src_user, dst_url, dst_token, dst_user, repo)
+
+"""
+Copies all the public repositories of given user on source server 
+under to destination authenticated user having dst_token on destination server. 
+Ownership of these repositories can manually be transfered to their 
+own users.
+
+Skips private user repositories.
+
+If dst_user and dst_token do not match, repositories are created under user 
+having dst_token and migration fails. If API allowed administrators to create 
+a repository under *any* user, we would not have this shortcoming.   
+"""
+def copy_user_repos(src_url, src_token, src_user, dst_url, dst_token, dst_user):
+    repos = get_user_repos(src_url, src_token, src_user)
+    for repo in repos:
+        repoName = repo['name']
+        copy_user_repo(src_url, src_token, src_user, dst_url, dst_token, dst_user, repoName)
+
+def print_repos(repos):
+    for repo in repos:
+        repoName = repo['name']
+        private = repo['private']
+        if private:
+            print "\t" + repoName + " (private)"
+        else:
+            print "\t" + repoName
 
 def print_orgs_and_repos(url, token):
     orgs = get_orgs(url, token)
     for org in orgs:
         orgName = org['username']
         print orgName
-        repos = get_repos(url, token, orgName)
-        for repo in repos:
-            repoName = repo['name']
-            print "\t" + repoName
+        print_repos(get_repos(url, token, orgName))
+
+def print_user_repos(url, token, user):
+    print user
+    print_repos(get_user_repos(url, token, user))
 
 parse_arguments()
-copy_all_orgs(arg_src_url, arg_src_token, arg_dst_url, arg_dst_token, arg_dst_owner_user)
-copy_all_repos(arg_src_url, arg_src_token, arg_dst_url, arg_dst_token)
+copy_orgs(arg_src_url, arg_src_token, arg_dst_url, arg_dst_token, arg_dst_owner_user)
+copy_repos(arg_src_url, arg_src_token, arg_dst_url, arg_dst_token)
